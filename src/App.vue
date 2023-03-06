32f0e7cf67e3f0fffffb0864a9fb922b2bfc2b97
Konv Suu <2583695112@qq.com>
2023-03-06 23:23:57 +0800

    feat: footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <div w-prose m-auto my-0>
     <Header />
+    <Footer />
   </div>
 </template>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hi</h1>
+  <p text="6 note-cl" py-4>
+    <span>Inspired by <a text-note-cl decoration-wavy href="https://github.com/ddiu8081/chatgpt-demo" target="_blank">Diu</a>, </span>
+    <span>Made by <a text-note-cl decoration-wavy href="https://github.com/kovsu" target="_blank">Kovsu</a>, </span>
+    <span>Powered by <a text-note-cl decoration-wavy href="https://openai.com/" target="_blank">OpenAI</a>.</span>
+  </p>
 </template>
